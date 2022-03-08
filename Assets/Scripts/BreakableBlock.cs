8622ff2301d6cd482053b46b1f6cc0df7a2ed840
DiegoK98 <diegsagdemig@hotmail.com>
2022-03-08 20:38:22 +0100

    Wall breaking with stone golem implemented

---
+++ Assets/Scripts/BreakableBlock.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableBlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Hitter"))
+            return;
+
+        if (other.transform.parent.TryGetComponent(out GolemBehaviour golem) && golem.currentMaterial == GolemMaterial.Stone)
+        {
+            // Break
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            // Play sound of hitting a wall
+        }
+    }
+}
