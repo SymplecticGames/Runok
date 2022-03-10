7bf9bad95cb44590af2374631b987f8e65319920
DiegoK98 <diegsagdemig@hotmail.com>
2022-03-10 20:03:36 +0100

    Refactored MobilePlatform for more generic aproach
    Now the platforming in AlphaLevelV2 can be fully played

--- Assets/Scripts/MobilePlatform.cs
+++ Assets/Scripts/MobilePlatform.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 
 public class MobilePlatform : MonoBehaviour
 {
+    private Transform _startingPos;
 
-    private float _startingPos;
-    private float _finalPos;
-    [SerializeField] private float _increment;
-    private float _currentInc;
+    [SerializeField]
+    private Transform _finalPos;
+
+    [SerializeField]
+    private float speed;
+
+    private float currentSpeed;
+
     private bool _movementStarted;
-    
+
+    private bool loopMovement;
+
+    private float lerpTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        _startingPos = transform.position.x;
-        _finalPos = transform.position.x - 7.0f;
+        _startingPos = this.transform.parent;
         _movementStarted = false;
+
+        lerpTime = 0.0f;
     }
 
     // Update is called once per frame
@@ -25,38 +35,61 @@ public class MobilePlatform : MonoBehaviour
     {
         if (_movementStarted)
         {
-            if (transform.position.x <= _finalPos) _currentInc = _increment;
-            if (transform.position.x >= _startingPos) _currentInc = -_increment;
-            transform.position = new Vector3(transform.position.x + _currentInc * Time.deltaTime, transform.position.y,
-                transform.position.z);
+            transform.position = Vector3.Lerp(_startingPos.position, _finalPos.position, lerpTime);
+
+            if (lerpTime > 1.0f)
+            {
+                if (loopMovement)
+                    currentSpeed = -speed;
+                else
+                    currentSpeed = 0.0f;
+
+                lerpTime = 1.0f;
+            }
+            else if (lerpTime < 0.0f)
+            {
+                if (loopMovement)
+                    currentSpeed = speed;
+                else
+                    currentSpeed = 0.0f;
+
+                lerpTime = 0.0f;
+            }
+            else
+                lerpTime += Time.deltaTime * currentSpeed;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Golem"))
-        {
-            other.GetComponent<GenericBehaviour>().SetAdditionalVel(new Vector3(_currentInc, 0.0f, 0.0f));
-        }
-    }
- 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Golem"))
-        {
-            other.GetComponent<GenericBehaviour>().SetAdditionalVel(Vector3.zero);
-        }
+        if (!other.CompareTag("Golem"))
+            return;
+
+        Vector3 velVector = currentSpeed * (_finalPos.position - _startingPos.position);
+
+        other.GetComponent<GenericBehaviour>().SetAdditionalVel(velVector);
     }
 
-    public void StartMovingPlatform()
+    private void OnTriggerExit(Collider other)
     {
+        if (!other.CompareTag("Golem"))
+            return;
+
+        other.GetComponent<GenericBehaviour>().SetAdditionalVel(Vector3.zero);
+    }
+
+    public void StartMovingPlatform(bool loop)
+    {
+        loopMovement = loop;
         StartCoroutine(MovePlatform());
     }
 
     private IEnumerator MovePlatform()
     {
         yield return new WaitForSeconds(1.0f);
-        _currentInc = _increment;
+        currentSpeed = speed;
         _movementStarted = true;
+
+        lerpTime = 0.0f;
     }
 }
