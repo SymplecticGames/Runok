37ee2293311d9ba2ae441b1af46a6b937003fcb9
GonzaloGNogales <gonzalognbusiness@gmail.com>
2022-04-13 13:57:40 +0200

    Fix build commit

--- Assets/Scripts/UI/SettingsUI.cs
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SettingsUI : MonoBehaviour
