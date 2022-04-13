4579374b19d284e07da40dadb1bb85cc017bf5d8
GonzaloGNogales <gonzalognbusiness@gmail.com>
2022-04-13 22:42:14 +0200

    Level 3 background added

--- Assets/Scripts/UI/SettingsUI.cs
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -1,9 +1,5 @@
 using System.Collections.Generic;
-using Cinemachine;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SettingsUI : MonoBehaviour
