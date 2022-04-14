06c5c78b34e6fb19f4cff6962e641e55916c133a
shykibi <anitawanche@gmail.com>
2022-04-14 19:10:31 +0200

    Momentary push

--- Assets/EndLevelUI.cs
+++ Assets/EndLevelUI.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Scene = UnityEditor.SearchService.Scene;
 
 public class EndLevelUI : MonoBehaviour
 {
+
+    public Text golemText;
+    public Text beetleText;
+    public Button button;
+    
+    private string[] _golemAbility = {"Material de plomo", "Material de madera"};
+    private string[] _beetleAbility = {"Disparo de luz", "Impulso de luz"};
+    
+    public void GoToHub()
+    {
+        Debug.Log("HOLA");
+        SceneTransition.instance.LoadScene("Hub");
+    }
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+        button.onClick.AddListener(GoToHub);
         
+        golemText.text = _golemAbility[ProgressManager.instance.currentLevel];
+        beetleText.text = _beetleAbility[ProgressManager.instance.currentLevel];
     }
 
     // Update is called once per frame
